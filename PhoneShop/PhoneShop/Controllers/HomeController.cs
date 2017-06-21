c25fd829b78d325745456c8fd582d787b8bd7269
Valerii <valeriikozhevnikov@gmail.com>
2017-06-21 13:03:43 +0300

    Вывод таблицы смартфонов

---
+++ PhoneShop/PhoneShop/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneShop.Models;
+
+namespace PhoneShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+    }
+}
